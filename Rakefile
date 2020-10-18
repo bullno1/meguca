0b554c308c0ae5e856e6df59ffa7f140a855dfae
Bach Le <bach@bullno1.com>
2020-10-18 15:53:21 +0800

    Create starter cards

--- Rakefile
+++ Rakefile
@@ -5,13 +5,15 @@ require 'rake/clean'
 CLEAN.include('_output/*').exclude('_output/.gitkeep')
 
 desc 'By default, just build the deck without extra options'
-task default: [:deck]
+task default: [:with_zone_guide, :deck]
 
 desc 'Build everything, with all the options'
 task all: [:with_pnp, :with_proofs, :deck]
 
 desc 'Build the deck'
-task(:deck)     { load 'src/deck.rb' }
+task(:deck)  do
+  load 'src/deck.rb'
+end
 
 desc 'Enable proof lines'
 task(:with_proofs) do
@@ -23,4 +25,9 @@ desc 'Enable print-and-play builds'
 task(:with_pnp) do
   puts "Enabling print-and-play builds."
   Squib.enable_build_globally :pnp
+end
+
+desc 'Enable zone guide lines'
+task(:with_zone_guide) do
+  Squib.enable_build_globally :with_zone_guide
 end
--- src/deck.rb
+++ src/deck.rb
@@ -1,11 +1,13 @@
 require 'squib'
 require 'game_icons'
+require 'fileutils'
 require_relative 'utils'
 
 # Tokens
 
 initiative_tokens = Squib.yaml file: 'data/initiative_tokens.yml'
 
+FileUtils.mkdir_p '_output/tokens'
 Squib::Deck.new(
   cards: initiative_tokens.nrows,
   **size_from_layout('layouts/initiative_tokens.yml', 'base')
@@ -32,7 +34,66 @@ Squib::Deck.new(
     layout: 'initiative_number'
   )
 
-  save_png prefix: 'initiative_token'
+  save_png prefix: 'tokens/initiative_'
 end
 
 # Starter deck
+
+starter_deck = Squib.yaml file: 'data/starter.yml'
+
+FileUtils.mkdir_p '_output/player_deck'
+Squib::Deck.new(
+  cards: starter_deck.nrows,
+) do
+  use_layout file: ['fantasy.yml', 'layouts/player_card.yml']
+
+  background color: 'white'
+  cut_zone radius: 0,  stroke_color: :white
+
+  build :with_zone_guide do
+    rect layout: 'title'
+    rect layout: 'art'
+    rect layout: 'description'
+    safe_zone radius: 0, stroke_color: :red
+  end
+
+  def basic_info(data:, attribute:, icon:, color:)
+    range = data[attribute].each_index.select do |i|
+      (data[attribute][i] || 0) > 0
+    end
+
+    svg(
+      range: range,
+      data: GameIcons.get(icon)
+                     .recolor(bg: 'ffffff00', fg: '000000')
+                     .string,
+      layout: attribute,
+      mask: color,
+    )
+
+    text str: data[attribute], layout: "#{attribute}_value"
+  end
+
+  basic_info(
+    data: starter_deck,
+    attribute: 'attack',
+    icon: 'lorc/swords-emblem',
+    color: 'red',
+  )
+  basic_info(
+    data: starter_deck,
+    attribute: 'coin',
+    icon: 'lorc/crystal-cluster',
+    color: 'yellow',
+  )
+  basic_info(
+    data: starter_deck,
+    attribute: 'move',
+    icon: 'delapouite/high-heel',
+    color: 'blue',
+  )
+
+  text str: starter_deck['title'], layout: 'title'
+
+  save_png prefix: 'player_deck/starter_', trim: '0.125in'
+end
